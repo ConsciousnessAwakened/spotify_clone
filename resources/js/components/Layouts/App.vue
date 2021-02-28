9dfa89e015830afa8ab3a6ff3c4b7f685c456484
Kim <kimdeguzman20@yahoo.com>
2021-02-28 11:11:48 +0800

    Clean
    - Remove sample tailwind page

--- resources/js/components/Layouts/App.vue
+++ resources/js/components/Layouts/App.vue
@@ -1,20 +1,17 @@
 <template>
     <div>
         <warping-cube :processing="page.processing"/>
-        <feature />
     </div>
 </template>
 
 <script>
-import Feature from "../Pages/Sample/Feature";
 import WarpingCube from "../Loaders/WarpingCube";
 
 export default {
     name: "App",
 
     components: {
-        WarpingCube,
-        Feature
+        WarpingCube
     },
 
     provide() {
@@ -37,7 +34,7 @@ export default {
         document.onreadystatechange = () => {
 
             if (DOCUMENT.READY_STATE.COMPLETE === document.readyState) {
-                that.page.processing = false;
+                that.page.processing = true;
             }
         }
     }
