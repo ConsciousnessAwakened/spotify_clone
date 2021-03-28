2276132b5a9f905fbb1fc5474cf620a7d52af16b
Kim <kimdeguzman20@yahoo.com>
2021-03-28 13:23:00 +0800

    Minor Refactor
    - Rename Header to Profile
    - Rename Profile to ProfileCard

--- resources/js/components/Pages/Main/Profile.vue
+++ resources/js/components/Pages/Main/Profile.vue
@@ -1,41 +1,49 @@
 <template>
-    <div class="rounded-b-md rounded-t-lg overflow-hidden border border-gray-200 shadow-lg m-4 p-2 max-w-xs bg-gradient-to-t from-transparent">
-        <img src="https://picsum.photos/300/100/" class="w-full rounded-t-lg" />
+    <section class="scaffoldDark">
+        <div class="container px-4 mx-auto">
+            <nav class="flex items-center">
 
-        <div class="flex px-4">
-            <div class="rounded-full w-2/4 h-auto bg-cover border-solid border-white border-4 -mt-10"
-                :style="{ 'background-image': 'url(' + profile + ')' }"></div>
-            <div class="ml-2 mt-1 w-full">
-                <div class="text-gray-500 text-xs" v-text=""></div>
-                <div class="text-gray-500 text-base font-semibold" v-text="displayName"></div>
-                <div class="h-px w-full bg-gradient-to-r from-gray-500 via-gray-400 to-transparent"></div>
-                <div>
-                    <span class="text-gray-400 text-sm font-bold" v-text="followers"></span>
-                    <span class="text-gray-400 text-sm">Followers</span>
+                <div class="relative py-2 ml-auto flex items-center space-x-3">
+                    <div class="relative">
+                        <div class="flex px-2 items-center rounded cursor-pointer text-gray-700 hover:text-bilberry-primary">
+                            <a class="text-base font-semibold">{{ followers }} Followers</a>
+                        </div>
+                    </div>
+                    <div class="h-12 bg-gradient-to-t from-transparent via-gray-500 from-transparent" style="width: 2px;">&nbsp;</div>
+                    <div class="bg-cover w-12 h-12 rounded-full cursor-pointer" :style="{'background-image': 'url(' + profileImage + ')'}" @click="profileDropDown = !profileDropDown">&nbsp;</div>
+                    <div class="relative">
+                        <div class="flex pr-2 items-center rounded cursor-pointer text-gray-700 hover:text-bilberry-primary" @click="profileDropDown = !profileDropDown">
+                            <a class="text-base font-semibold">{{ displayName }}</a>
+
+                            <svg class="w-5 h-5 ml-2 transform transition-transform duration-200" :class="{'rotate-180': profileDropDown, 'rotate-0' : !profileDropDown}" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                            </svg>
+                        </div>
+
+                        <div v-if="profileDropDown" class="origin-top-right border-gray-400 border absolute right-0 mt-1 w-36 bg-white focus:outline-none rounded-lg">
+                            <a :href="accountUrl" target="_blank" class="px-2 py-1 block text-sm font-semibold text-gray-700 hover:bg-bilberry-dark hover:text-gray-100 rounded-lg">View Profile</a>
+                            <inertia-link href="/logout" class="px-2 py-1 block text-sm font-semibold text-gray-700 hover:bg-bilberry-dark hover:text-gray-100 rounded-lg">Sign out</inertia-link>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </nav>
         </div>
-    </div>
+    </section>
 </template>
 
 <script>
-import {mapGetters} from "vuex";
-
 export default {
     name: "Profile",
 
-    computed : {
-        ...mapGetters('account',{
-            displayName : 'name',
-            followers : 'followers',
-            account_url : 'url',
-            profile : 'profile'
-        })
+    data() {
+        return {
+            profileDropDown : false
+        }
     },
 
     mounted() {
-        this.$store.dispatch('account/getData');
-    },
+        this.getAccount();
+    }
 }
 </script>
 
